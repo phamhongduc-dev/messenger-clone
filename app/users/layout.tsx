ace1f145f9bcab945ea45e0e19ff097f32ccf767
pham_duc <duc.pham@beetech.com.vn>
2023-06-05 14:09:05 +0700

    [wip]: 3:11

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,20 @@
 import Sidebar from "../(site)/components/sidebar/Sidebar";
+import getUsers from "../actions/getUsers";
+import UserList from "./UserList";
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const users = getUsers();
   return (
     // @ts-expect-error Sever Comopnents
     <Sidebar>
-      <div className=" h-full">{children}</div>
+      <div className=" h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 }
