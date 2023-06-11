cc5b4fe921db8793f67c30f2fdfa11dcd80e2f52
pham_duc <duc.pham@beetech.com.vn>
2023-06-12 00:39:54 +0700

    04:12:34 Messages creation, Message Image upload

--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -12,9 +12,9 @@ const useRoutes = () => {
     const routes = useMemo(() => [
         {
             label: 'Chat',
-            href: '/conversation',
+            href: '/conversations',
             icon: HiChat,
-            active: pathName === '/conversation' || !!conversationId
+            active: pathName === '/conversations' || !!conversationId
         }, {
             label: 'Users',
             href: '/users',
